09441c7cd0ff27ed50111969715b4c50e174fbed
胡浩 <mousycoder@foxmail.com>
2019-04-27 18:44:19 +0800

    ReentranLock测试

---
+++ src/main/java/com/mousycoder/mycode/concurrence/ReentranLockTest.java
@@ -0,0 +1,44 @@
+package com.mousycoder.mycode.concurrence;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @version 1.0
+ * @author: mousycoder
+ * @date: 2019-04-27 18:26
+ */
+public class ReentranLockTest {
+
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+
+
+        lock.lock();
+
+        try {
+            System.out.println("begin wait");
+            condition.await();
+            System.out.println("end wait");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+
+        lock.lock();
+
+        try {
+            System.out.println("begin signal");
+            condition.signal();
+            System.out.println("end signal");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+
+
+    }
+}
